c694c94495582f241a0397f7df2d78185bfc9118
Joseph Fahey <fahey.joseph@gmail.com>
2015-01-30 11:01:31 +0100

    project file getting up to snuff

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371" :scope "provided"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [ring "1.3.1"]
                  [compojure "1.2.0"]
                  [enlive "1.1.5"]
@@ -16,10 +17,12 @@
                  [environ "1.0.0"]
                  [com.cemerick/piggieback "0.1.3"]
                  [weasel "0.4.0-SNAPSHOT"]
-                 [leiningen "2.5.0"]]
+                 [leiningen "2.5.0"]
+                 [cljs-xml "0.1.0-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-environ "1.0.0"]]
+            [lein-environ "1.0.0"]
+            [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
 
   :min-lein-version "2.5.0"
 
